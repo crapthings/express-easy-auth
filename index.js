f01854fc4942f2ba62147c65973e769c77743f9b
crapthings <crapthings@gmail.com>
2019-08-29 17:51:03 +0800

    minor update

--- index.js
+++ index.js
@@ -12,16 +12,19 @@ const bodyParser = require('body-parser')
 const server = express()
 const router = express.Router()
 
+const registerDatabse = require('./db')
+const registerRoutes = require('./routes')
+
 server.use(cors())
 server.use(bodyParser.urlencoded({ extended: false }))
 server.use(bodyParser.json())
 
-const startup = async function () {
-  const { db, authCollection } = await require('./db')({ ENV_MONGO_URL })
-  server.use('/api', require('./routes')({ db, authCollection, router }))
+const registerServer = async function () {
+  const { db, authCollection } = await registerDatabse({ ENV_MONGO_URL })
+  server.use('/api', registerRoutes({ db, authCollection, router }))
   server.listen(ENV_PORT, function () {
     console.log(`Express-Easy-Auth is listening on port`, ENV_PORT)
   })
 }
 
-startup()
+registerServer()
